4fa497dfa6bee8fe5d748c8d84fce5a9f0dee926
ncavchl <48319693+ncavchl@users.noreply.github.com>
2023-12-04 16:55:54 +0900

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,25 @@
 // nodemon.json - 노드몬이 실행될 때 어떤 걸 변경 감지사항 삼아야할지, 말아야 할지 + 환경 정보 세팅
+import express from "express";
+import cors from "cors";
+import messagesRoute from "./routes/messages.js";
+import usersRoute from "./routes/users.js";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+const routes = [...messagesRoute, ...usersRoute];
+routes.forEach(({ method, route, handler }) => {
+  app[method](route, handler);
+});
+
+app.listen(8000, () => {
+  console.log("server listening on 8000...");
+});
