88e5451f6e7eed159bf3c6acd2c2c15f55174180
ncavchl@hyundai-autoever.com <ncavchl@hyundai-autoever.com>
2023-12-06 14:23:18 +0900

    GraphQl 기본 세팅 - resolver, schema 설정

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,35 @@
 // nodemon.json - 노드몬이 실행될 때 어떤 걸 변경 감지사항 삼아야할지, 말아야 할지 + 환경 정보 세팅
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
 import cors from "cors";
-import messagesRoute from "./routes/messages.js";
-import usersRoute from "./routes/users.js";
+
+import resolvers from "./resolvers/index.js";
+import schema from "./schema/index.js";
+import { readDB } from "./dbController.js";
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers: resolvers, // route에서 하던 역할
+  context: {
+    db: {
+      messages: readDB("messages"),
+      users: readDB("users"),
+    },
+  },
+});
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
+await server.start();
+
+server.applyMiddleware({
+  app,
+  path: "/graphql",
+  cors: {
+    origin: ["http://localhost:3000", "https://studio.apollographql.com"],
     credentials: true,
-  })
-);
-
-const routes = [...messagesRoute, ...usersRoute];
-routes.forEach(({ method, route, handler }) => {
-  app[method](route, handler);
+  },
 });
 
 app.listen(8000, () => {
